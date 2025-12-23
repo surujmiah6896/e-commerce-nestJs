c32bc72fa91cc4a06470d7dc210131e159634d9f
surujmiah6896 <surujmiah6896@gmail.com>
2025-12-23 13:04:54 +0600

    create user module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,7 +1,30 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserController } from './user.controller';
+import { JwtStrategy } from '../auth/strategies/jwt.strategy';
 
 @Module({
-  providers: [UsersService]
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService)=>({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: {
+          expiresIn: configService.get<string>('jwt.expiresIn'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [UserController],
+  providers: [UsersService, JwtStrategy],
+  exports: [UsersService, JwtModule]
 })
 export class UsersModule {}
