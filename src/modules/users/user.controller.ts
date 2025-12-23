9e90b6a22a917aa22ee49e0c6b8bcb41adc623e2
surujmiah6896 <surujmiah6896@gmail.com>
2025-12-23 12:44:38 +0600

    create common decorator

---
+++ src/modules/users/user.controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { UsersService } from "./users.service";
+
+// @ApiTags('users')
+@Controller('users')
+export class UserController {
+    constructor(private readonly userService: UsersService){}
+
+    
+}
